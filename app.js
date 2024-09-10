1364f7f10dbe765c6e6fe1de4d7d179a750482d9
Vikrant <vikrant.kaloniya1993@gmail.com>
2024-09-11 00:44:00 +0530

    update

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const apiRoutes = require("./Routes/apiRoutes");
-const multer = require("multer");
 
-
-const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3006;
 const app = express();
-
-const storage = multer.memoryStorage(); 
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }, 
-});
+var path = require("path");
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -23,6 +15,8 @@ app.use("/apis",apiRoutes);
 app.get('/', (req, res) => {
   res.send('Hello, this is your Express server!');
 });
+
+app.use('/privacy_policy',express.static(__dirname+'/privacy_policy.html'));
 
 
 app.listen(port, () => {
