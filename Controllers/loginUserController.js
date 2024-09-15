32d5a2a76edc6cd3369cdc3e0f29a4ed356f5130
Vikrant <vikrant.kaloniya1993@gmail.com>
2024-09-15 15:07:26 +0530

    1.10

--- Controllers/loginUserController.js
+++ Controllers/loginUserController.js
@@ -7,25 +7,23 @@ const loginUser = (req, res) => {
   console.log(Username, Password);
 
   if (!Username || !Password) {
-    return res
-      .status(400)
-      .json({ status: false, data: null, message: "Required Username and Password" });
+    return res.json({ status: false, data: null, message: "Required Username and Password" });
   }
 
   const sql = "SELECT * FROM CMSDB.Login WHERE Username = ? AND Password = ?";
   db.query(sql, [Username, Password], (err, result) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ status: false, data: null, message: "Internal Server Error" });
+      return res.json({ status: false, data: null, message: "Internal Server Error" });
     }
     if (result.length === 1) {
       // Authentication successful
-      return res.status(200).json({
+      return res.json({
         status: true, data: result[0], message: "Login Successful"
       });
     } else {
       // Authentication failed
-      return res.status(401).json({ status: false, data: null, message: "Invalid Credentials" });
+      return res.json({ status: false, data: null, message: "Invalid Credentials" });
     }
   });
 };
