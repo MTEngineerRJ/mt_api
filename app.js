f909df8426089361f3b9178b1b43deb59d7e1544
Vikrant <vikrant.kaloniya1993@gmail.com>
2025-01-22 19:48:12 +0530

    added redirect rule for the app

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ var path = require("path");
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use("/apis",apiRoutes);
+app.use("/apis", apiRoutes);
+
+app.get("/leadDetails/:id", (req, res) => {
+  res.redirect('https://play.google.com/store/apps/details?id=vk.apps.mte&leadDetails=' + req.params.id)
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your Express server!');
 });
 
-app.use('/privacy_policy',express.static(__dirname+'/privacy_policy.html'));
-app.use('/.well-known/assetlinks.json',express.static(__dirname+'/.well-known/assetlinks.json'));
+app.use('/privacy_policy', express.static(__dirname + '/privacy_policy.html'));
+app.use('/.well-known/assetlinks.json', express.static(__dirname + '/.well-known/assetlinks.json'));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
