41700f816e723d90aa6b8bb47254f9fe77311c25
Vikrant <vikrant.kaloniya1993@gmail.com>
2025-05-09 22:38:18 +0530

    list update

--- Controllers/claimsController.js
+++ Controllers/claimsController.js
@@ -3,11 +3,12 @@ const db = require("../Config/dbConfig");
 const getAllClaims = (req, res) => {
   // const Region1 = req.query.Region
   const token = req.headers["token"]
-  const CalimStatus = 1
+  const CalimStatus = '1,2,3,4,5,6,7,8,9,10,11,12,13'
 
-  const userRegions = "SELECT Region1, Region2, Region3, Region4, Region5, Region6 FROM CMSDB.Login WHERE UserID = ?"
+  const userRegions = "SELECT Username,Region1, Region2, Region3, Region4, Region5, Region6 FROM Login WHERE UserID = ?"
 
-  const sql = "CALL GetPolicyInfoByRegions(?, ?, ?, ?, ?, ?, ?, ?)";
+  const sql = "CALL GetPolicyInfoByRegionsUpdated(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  // const sql = "CALL GetPolicyInfoByRegions(?, ?, ?, ?, ?, ?, ?, ?)";
 
   // if (!Region1) {
   //   return res.json({ status: false, data: null, message: "invalid region" });
@@ -18,6 +19,7 @@ const getAllClaims = (req, res) => {
       console.error(err);
       return res.json({ status: false, data: null, message: "Internal Server Error" });
     }
+    //     return res.json({ status: false, data: loginResult[0], message: "Internal Server Error" });
     if (loginResult.length === 1) {
       const params = [
         loginResult[0].Region1 || null,
@@ -26,8 +28,9 @@ const getAllClaims = (req, res) => {
         loginResult[0].Region4 || null,
         loginResult[0].Region5 || null,
         loginResult[0].Region6 || null,
+        'Spot',
         CalimStatus,
-        null
+        loginResult[0].Username || null,
       ];
 
       db.query(sql, params, (err, result) => {
@@ -35,7 +38,7 @@ const getAllClaims = (req, res) => {
           console.error(err);
           return res.json({ status: false, data: null, message: "Internal Server Error" });
         }
-        return res.json({ status: true, data: result[0].reverse(), message: "survey found" });
+        return res.json({ status: true, data: result[0].reverse(),length: result[0].length,user:loginResult[0], message: "survey found" });
       });
     }
   });
