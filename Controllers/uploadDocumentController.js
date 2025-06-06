7e03d8b71bafdebb808d536b39c2c360264cf9a1
MTEngineerRJ <161291220+MTEngineerRJ@users.noreply.github.com>
2025-06-06 20:07:23 +0530

    Update uploadDocumentController.js
    satatus with docoment

--- Controllers/uploadDocumentController.js
+++ Controllers/uploadDocumentController.js
@@ -2,13 +2,15 @@ const db = require("../Config/dbConfig");
 
 const getDocuments = (req, res) => {
   const LeadId = req.query.LeadId;
-  const sql = "SELECT LeadID, DocumentName, Photo1 as FileUrl, Attribute1 as FileName, Photo1Latitude as Latitude, Photo1Longitude as Longitude, Photo1Timestamp as Timestamp FROM DocumentList WHERE LeadID = ?;";
-
+  //const sql = "SELECT LeadID, DocumentName, Photo1 as FileUrl, Attribute1 as FileName, Photo1Latitude as Latitude, Photo1Longitude as Longitude, Photo1Timestamp as Timestamp FROM DocumentList WHERE LeadID = ?;";
+  
+  const sql = "SELECT DL.LeadID, DL.DocumentName, DL.Photo1 AS FileUrl, DL.Attribute1 AS FileName, DL.Photo1Latitude AS Latitude, DL.Photo1Longitude AS Longitude, DL.Photo1Timestamp AS Timestamp FROM DocumentList DL WHERE DL.LeadID=? AND EXISTS ( SELECT 1 FROM ClaimStatus CS WHERE CS.LeadID = DL.LeadID AND (CS.Status = 1 OR CS.LeadID = ?));";
+  
   if (!LeadId) {
     return res.json({ status: false, data: null, message: "invalid lead id" });
   }
 
-  db.query(sql, [LeadId], (err, result) => {
+  db.query(sql, [LeadId,LeadId], (err, result) => {
     if (err) {
       return res.json({ status: false, data: null, message: "Internal Server Error" });
     }
